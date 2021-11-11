e389523258eb63daedc1aefb5f0af2d8cada62ac
Sergei <cot140194@mail.ru>
2021-11-11 05:03:26 +0300

    Change text of run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,6 @@
 #2.
 #Команда №2 сработает если постгря поднята и в app.yaml указана переменная окружения:
 docker run -ti --rm -e DATASOURCE_HOST=192.168.56.1 -p 8081:8080 koshachek/cats-api:1.0.0
-
 #для mac: docker run -ti --rm -e DATASOURCE_HOST=localhost -p 8081:8080 koshachek/cats-api:1.0.0
 
 #Вход в swagger-ui.html по http://192.168.99.100:8081/swagger-ui.html
